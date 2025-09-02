e7cfb93f3c5495291e9cb6608d6cd0eee8ef508d
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2025-09-02 01:26:58 -0300

    add: effect tags

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const ELEMENT_TYPES = {
   TEXT: 'TEXT_ELEMENT'
 }
 
+const EFFECTS = {
+  UPDATE: 'UPDATE',
+  PLACEMENT: 'PLACEMENT',
+  DELETION: 'DELETION'
+}
+
 // Top level API definition
 const Pipot = {
   createElement,
@@ -36,17 +42,30 @@ function createTextElement(text) {
 }
 
 function commitRoot() {
+  deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  currentRoot = wipRoot
   wipRoot = null
 }
 
 function commitWork(fiber) {
+  // commit phase
   if (!fiber) return
+  const { effectTag, dom, alternate, props, parent, child, sibling } = fiber
+  const domParent = parent.dom
 
-  const domParent = fiber.parent.dom
-  domParent.appendChild(fiber.dom)
-  commitWork(fiber.child)
-  commitRoot(fiber.sibling)
+  // apply effects
+
+  if (effectTag === EFFECTS.PLACEMENT && dom != null) {
+    domParent.appendChild(dom)
+  } else if (effectTag === EFFECTS.UPDATE && dom != null) {
+    updateDom(dom, alternate.props, props)
+  } else if (effectTag === EFFECTS.DELETION) {
+    domParent.removeChild(dom)
+  }
+
+  commitWork(child)
+  commitWork(sibling)
 }
 
 function buildDom(fiber) {
@@ -66,20 +85,67 @@ function buildDom(fiber) {
   return dom
 }
 
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && isEvent(key)
+const isGone = (_, next) => key => !(key in next)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+
+function updateDom(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => {
+      return !(key in nextProps) || isNew(prevProps, nextProps)(key)
+    })
+    .forEach(name => {
+      const eventType = name.toLocaleLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ''
+    })
+
+  // Set new or updated properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLocaleLowerCase().substring(2) // remove 'on'
+      dom.addEventListener(eventType, nextProps[name])
+    })
+}
+
 function render(element, container) {
   // Root fiber is created here
   wipRoot = {
     dom: container, // DOM node reference
     props: {
       children: [element]
-    }
+    },
+    alternate: currentRoot
   }
 
+  deletions = []
   nextUnitOfWork = wipRoot // once nextUnitOfWork is not null, work starts
 }
 
 let nextUnitOfWork = null
 let wipRoot = null
+let currentRoot = null
+let deletions = null
 
 function workLoop(deadline) {
   // Will yield to the browser if idle time remaining is less than 1ms
@@ -87,37 +153,19 @@ function workLoop(deadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
   }
 
+  // commit phase starts
   if (!nextUnitOfWork && wipRoot) commitRoot()
 
   requestIdleCallback(workLoop)
 }
+requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
   // Add DOM node
   if (!fiber.dom) fiber.dom = buildDom(fiber)
 
-  // Create new fibers
-  const children = fiber.props.children
-  let index = 0
-  let prevSibling = null
-
-  while (index < children.length) {
-    // create child fibers
-    const element = children[index]
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null
-    }
-
-    if (index === 0) fiber.child = newFiber
-    else prevSibling.sibling = newFiber
-
-    prevSibling = newFiber
-    index++
-  }
+  // perform reconciliation
+  reconcileChildren(fiber, fiber.props.children)
 
   // return the next unit of work
   if (fiber.child) return fiber.child
@@ -129,7 +177,56 @@ function performUnitOfWork(fiber) {
   }
 }
 
-requestIdleCallback(workLoop)
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+
+  while (index < elements.length || oldFiber != null) {
+    // The element is the thing we want to render to the DOM and the oldFiber is what we rendered the last time.
+    const element = elements[index]
+    let newFiber = null
+
+    const sameType = oldFiber && element && element.type == oldFiber.type
+
+    if (sameType) {
+      // update node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: EFFECTS.UPDATE // used during commit phase
+      }
+    }
+
+    if (element && !sameType) {
+      // replace dom node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: EFFECTS.PLACEMENT
+      }
+    }
+
+    if (oldFiber && !sameType) {
+      // delete oldFiber node
+      oldFiber.effectTag = EFFECTS.DELETION
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) oldFiber = oldFiber.sibling
+    if (index === 0) wipFiber.child = newFiber
+    else prevSibling.sibling = newFiber
+
+    prevSibling = newFiber
+    index++
+  }
+}
 
 /** @jsx Pipot.createElement */
 // The above line indicates babel to use Pipot.createElement to handle JSX
